cf32501905246de719638a3a0cd1f3ad5634ad8c
sakibdeveloper999 <arjunsakib43@gmail.com>
2025-11-28 16:31:01 +0600

    first commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Navbar.css'
+import logo from '../../assets/logo.png'
+
+const Navbar = () => {
+  return (
+    <nav>
+        <img src={logo} alt="" />
+        <ul>
+            <li>Home</li>
+            <li>Program</li>
+            <li>About us </li>
+            <li>Campus</li>
+            <li>Testimonials</li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
