6ca01744850feaea31b948133c24b555e37bc40c
sakibdeveloper999 <arjunsakib43@gmail.com>
2025-11-30 13:42:08 +0600

    Add all device responsive Navbar menu ( onClick function and media css)

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.png'
+import menu_icon from '../../assets/menu-icon.png'
 import { Link } from 'react-scroll';
 
 const Navbar = () => {
@@ -9,18 +10,24 @@ const Navbar = () => {
         window.addEventListener('scroll',()=>{
             window.scrollY > 50 ? setSticky(true) : setSticky(false)
         })
-    },[])
+    },[]);
+
+    const [mobileMenu, setMobileMenu] = useState(false)
+    const toggleMenu =()=>{
+        mobileMenu ? setMobileMenu(false) : setMobileMenu(true);
+    }
   return (
     <nav className={`container ${sticky ? 'dark-nav': ''}`} >
         <img src={logo} alt="" className='logo'/>
-        <ul>
+        <ul className={mobileMenu ? '':'hide-mobile-menu'}>
             <li><Link to='hero' smooth={true} offset={0} duration={500}>Home</Link></li>
             <li><Link to='program' smooth={true} offset={-260} duration={500}>Program</Link></li>
             <li><Link to='about' smooth={true} offset={-150} duration={500}>About us </Link></li>
             <li><Link to='campus' smooth={true} offset={-260} duration={500}>Campus</Link></li>
             <li><Link to='testimonials' smooth={true} offset={-260} duration={500}>Testimonials</Link></li>
-            <li><Link to='contact' smooth={true} offset={-260} duration={500} className='btn'>Contact Us</Link></li>
+            <li><Link to='contact' smooth={true} offset={-260} duration={500} className='btn btn-nav'>Contact Us</Link></li>
         </ul>
+        <img onClick={toggleMenu} src={menu_icon} className='menu-icon' alt="" />
     </nav>
   )
 }
